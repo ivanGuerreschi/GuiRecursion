b7d6bc466f6d5a076b336a492c285584cc2a9c0f
Ivan Guerreschi <ivanguerreschi86@gmail.com>
2020-07-12 09:01:24 +0200

    astyle gnu

--- src/main.c
+++ src/main.c
@@ -4,9 +4,9 @@
 
 static void
 on_click_button (GtkEntry   *widget,
-		 AppWidgets *widgets)
+                 AppWidgets *widgets)
 {
-  const gchar *get_number = gtk_entry_get_text ((GTK_ENTRY (widgets->entry_input)));  
+  const gchar *get_number = gtk_entry_get_text ((GTK_ENTRY (widgets->entry_input)));
   const gchar *stringa = g_strdup_printf ("%i", perimeter (atoi (get_number)));
   gtk_entry_set_text (GTK_ENTRY (widgets->entry_output), stringa);
 }
@@ -20,12 +20,11 @@ activate (GtkApplication *app,
   GtkWidget *button;
   GtkWidget *label;
   AppWidgets *widgets = g_slice_new (AppWidgets);
-   
-  /* create a new window, and set its title */
+
   window = gtk_application_window_new (app);
   gtk_window_set_title (GTK_WINDOW (window), "Window");
   gtk_container_set_border_width (GTK_CONTAINER (window), 10);
-  
+
   grid = gtk_grid_new ();
 
   gtk_container_add (GTK_CONTAINER (window), grid);
@@ -41,7 +40,7 @@ activate (GtkApplication *app,
 
   widgets->entry_output = gtk_entry_new ();
   gtk_grid_attach (GTK_GRID (grid), widgets->entry_output, 1, 1, 1, 1);
-  
+
   button = gtk_button_new_with_label ("Calculate");
   g_signal_connect (button, "clicked", G_CALLBACK (on_click_button), widgets);
   gtk_grid_attach (GTK_GRID (grid), button, 0, 2, 1, 1);
@@ -49,7 +48,7 @@ activate (GtkApplication *app,
   button = gtk_button_new_with_label ("Quit");
   g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
   gtk_grid_attach (GTK_GRID (grid), button, 1, 2, 1, 1);
-  
+
   gtk_widget_show_all (window);
 }
 
@@ -59,11 +58,11 @@ main (int    argc,
 {
   GtkApplication *app;
   int status;
-  
+
   app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
   g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
   status = g_application_run (G_APPLICATION (app), argc, argv);
   g_object_unref (app);
-    
+
   return status;
 }
